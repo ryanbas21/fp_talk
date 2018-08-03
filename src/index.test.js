8cb0f1ac6070827cdda75c44968a71a31ef22ec7
ryanbas21 <ryanbas21@gmail.com>
2018-08-02 23:02:56 -0500

    update

--- src/index.test.js
+++ src/index.test.js
@@ -7,7 +7,7 @@ test("Refactor words to be partially applied", () => {
 
 test("Curried add", () => {
   expect(add(1)(2)).toEqual(3);
-  expect(add(2)(4).toEqual(6));
+  expect(add(2)(4)).toEqual(6);
 });
 
 test("Compose should apply functions from right to left", () => {
